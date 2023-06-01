9c969e2109f69473cdd146e58031a4940d425117
Connor Robetorye <crobetorye@gmail.com>
2023-06-01 10:39:22 -0700

    laplacian regularaized gmm

--- sklearn/mixture/__init__.py
+++ sklearn/mixture/__init__.py
@@ -4,6 +4,6 @@ The :mod:`sklearn.mixture` module implements mixture modeling algorithms.
 
 from ._gaussian_mixture import GaussianMixture
 from ._bayesian_mixture import BayesianGaussianMixture
+from ._gaussian_mixture_reg import LapRegGaussianMixture
 
-
-__all__ = ["GaussianMixture", "BayesianGaussianMixture"]
+__all__ = ["GaussianMixture", "BayesianGaussianMixture", "LapRegGaussianMixture"]
